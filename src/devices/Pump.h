0a9ecd942215e8803eb57f51e7f7f41732fe45d0
Niels Dehio <niels.dehio@mailbox.org>
2020-07-14 18:12:11 +0200

    add functions related to sensor and actuator

--- src/devices/Pump.h
+++ src/devices/Pump.h
@@ -46,84 +46,171 @@ struct MC_RBDYN_DLLAPI Pump : public mc_rbdyn::Device
     return Device::X_p_s();
   }
 
-  //METHODS RELATED TO SENSOR SIGNALS => https://github.com/frankaemika/libfranka/blob/master/include/franka/vacuum_gripper_state.h
+  // #################################
+  // METHODS RELATED TO SENSOR SIGNALS => https://github.com/frankaemika/libfranka/blob/master/include/franka/vacuum_gripper_state.h
+  // #################################
 
   /** Check if the vacuum value is within in setpoint area */
-  inline const bool & in_control_range() const
+  inline const bool & get_in_control_range() const
   {
     return in_control_range_;
   }
 
+  inline void set_in_control_range(bool in_control_range)
+  {
+    in_control_range_ = in_control_range;
+  }
+
   /** Check if part has been detached after a suction cycle */
-  inline const bool & part_detached() const
+  inline const bool & get_part_detached() const
   {
     return part_detached_;
   }
+
+  inline void set_part_detached(bool part_detached)
+  {
+    part_detached_ = part_detached;
+  }
   
   /** Check if vacuum is over H2 and not yet under H2-h2 */
-  inline const bool & part_present() const
+  inline const bool & get_part_present() const
   {
     return part_present_;
   }
   
-  /** Return the current vacuum gripper device status */
-  inline const uint8_t & device_status() const
+  inline void set_part_present(bool part_present)
   {
-    return device_status_;
+    part_present_ = part_present;
+  }
+
+  /** Return the current vacuum gripper device status */
+  inline const bool & get_device_status_ok() const
+  {
+    return device_status_ok_;
+  }
+
+  inline void set_device_status_ok(bool device_status_ok)
+  {
+    device_status_ok_ = device_status_ok;
   }
 
   /** Return the current vacuum gripper actual power. Unit: \f$[%]\f$ */
-  inline const uint16_t & actual_power() const
+  inline const uint16_t & get_actual_power() const
   {
     return actual_power_;
   }
 
+  inline void set_actual_power(uint16_t actual_power)
+  {
+    actual_power_ = actual_power;
+  }
+
   /** Return the current system vacuum. Unit: \f$[mbar]\f$ */
-  inline const uint16_t & vacuum() const
+  inline const uint16_t & get_vacuum() const
   {
     return vacuum_;
   }
 
-  //METHODS RELATED TO ACTUATOR COMMANDS => https://github.com/frankaemika/libfranka/blob/master/include/franka/vacuum_gripper.h
+  inline void set_vacuum(uint16_t vacuum)
+  {
+    vacuum_ = vacuum;
+  }
+
+  // ####################################
+  // METHODS RELATED TO ACTUATOR COMMANDS => https://github.com/frankaemika/libfranka/blob/master/include/franka/vacuum_gripper.h
+  // ####################################
 
   /** Vacuum an object. Return true if the vacuum has been established, otherwise return false.
    * @param[in] vacuum Setpoint for control mode. Unit: \f$[10*mbar]\f$.
    * @param[in] timeout Vacuum timeout. Unit: \f$[ms]\f$.
    */
-  inline bool & vacuum(const uint8_t vacuum, const std::chrono::milliseconds timeout)
+  inline void requestVacuumCommand(const uint8_t vacuum, const std::chrono::milliseconds timeout)
   {
-    return vacuumSuccessful_;
+    vacuumCommand_ = vacuum;
+    vacuumTimeoutCommand_ = timeout;
+    vacuumCommandRequested_ = true;
+    // return vacuumSuccessful_;
+  }
+
+  inline const bool & vacuumCommandRequested() const
+  {
+    return vacuumCommandRequested_;
+  }
+
+  inline const void getVacuumCommandParams(uint8_t & vacuum, std::chrono::milliseconds & timeout) const
+  {
+    vacuum = vacuumCommand_;
+    timeout = vacuumTimeoutCommand_;
+  }
+
+  inline void setVacuumCommandResult(const bool ok)
+  {
+    vacuumSuccessful_ = ok;
   }
 
   /** Drop the grasped object off. Return true if command was successful, otherwise return false.
    * @param[in] timeout Dropoff timeout. Unit: \f$[ms]\f$.
    */
-  inline bool & dropOff(const std::chrono::milliseconds timeout)
+  inline void requestDropoffCommand(const std::chrono::milliseconds timeout)
   {
-    return dropOffSuccessful_;
+    dropoffTimeoutCommand_ = timeout;
+    dropoffCommandRequested_ = true;
+    // return dropoffSuccessful_;
+  }
+
+  inline const bool & dropoffCommandRequested() const
+  {
+    return dropoffCommandRequested_;
+  }
+
+  inline const void getDropoffCommandParam(std::chrono::milliseconds & timeout) const
+  {
+    timeout = dropoffTimeoutCommand_;
+  }
+
+  inline void setDropoffCommandResult(const bool ok)
+  {
+    dropoffSuccessful_ = ok;
   }
 
   /** Stop a currently running vacuum or drop off operation. Return true if command was successful, otherwise return false.*/
-  inline bool & stop()
+  inline void requestStopCommand()
   {
-    return stopSuccessful_;
+    stopCommandRequested_ = true;
+    // return stopSuccessful_;
+  }
+
+  inline const bool & stopCommandRequested() const
+  {
+    return stopCommandRequested_;
+  }
+
+  inline void setStopCommandResult(const bool ok)
+  {
+    stopSuccessful_ = ok;
   }
 
   mc_rbdyn::DevicePtr clone() const override;
 
 private:
-  //sensor signals
+  //sensor signal related members
   bool in_control_range_; // Vacuum value within in setpoint area.
-  bool part_detached_; // The part has been detached after a suction cycle
+  bool part_detached_; // The part has been detached after a suction cycle.
   bool part_present_; // Vacuum is over H2 and not yet under H2-h2. For more information check the cobot-pump manual.
-  uint8_t device_status_; //Current vacuum gripper device status.
+  bool device_status_ok_; //Current vacuum gripper device status.
   uint16_t actual_power_; // Current vacuum gripper actual power. Unit: \f$[%]\f$.
   uint16_t vacuum_; // Current system vacuum. Unit: \f$[mbar]\f$.
 
-  //actuator command feedback
-  bool vacuumSuccessful_;
-  bool dropOffSuccessful_;
-  bool stopSuccessful_;
+  //actuator command related members
+  uint8_t vacuumCommand_ = 0; // Setpoint for control mode. Unit: \f$[10*mbar]\f$.
+  std::chrono::milliseconds vacuumTimeoutCommand_ = std::chrono::milliseconds(0); // Vacuum timeout. Unit: \f$[ms]\f$.
+  std::chrono::milliseconds dropoffTimeoutCommand_ = std::chrono::milliseconds(0); // Dropoff timeout. Unit: \f$[ms]\f$.
+  bool vacuumCommandRequested_ = false;
+  bool dropoffCommandRequested_ = false;
+  bool stopCommandRequested_ = false;
+  bool vacuumSuccessful_ = false;
+  bool dropoffSuccessful_ = false;
+  bool stopSuccessful_ = false;
 };
 
 typedef std::vector<Pump, Eigen::aligned_allocator<Pump>> PumpVector;
