60f9d39e47dd0e146246203f2ddaea132522341d
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-07 13:56:08 +0200

    tests: Update test, but disable them for now
    Tests would need a local version of the robot module to be generated in
    the build folder. Ignoring for now

--- tests/test_linking.cpp
+++ tests/test_linking.cpp
@@ -1,10 +1,10 @@
 /** This creates an arbitrary panda module, the goal is to make sure there is
  * no undefined symbol in the generated RobotModule library */
 
-#include <panda.h>
+#include <mc_panda/panda.h>
 
 int main()
 {
-  mc_panda::PandaRobotModule robot(true, false, false);
+  mc_panda::PandaRobotModule robot("panda_test_linking", true, false, false);
   return 0;
 }
--- tests/test_module.cpp
+++ tests/test_module.cpp
@@ -1,18 +1,36 @@
 #include <mc_rbdyn/RobotLoader.h>
+#include <mc_panda/panda.h>
+#include <mc_rtc/logging.h>
 
 #include "test_module.h"
+
+using namespace mc_panda;
 
 int main()
 {
   mc_rbdyn::RobotLoader::clear();
   mc_rbdyn::RobotLoader::update_robot_module_path({MODULE_DIR});
-  for(const auto & name : {"PandaDefault", "PandaHand", "PandaFoot", "PandaPump"})
-  {
-    auto robot = mc_rbdyn::RobotLoader::get_robot_module(name);
-    if(robot->mb.nrDof() == 0)
-    {
-      return 1;
-    }
-  }
+
+  ForAllVariants([](PandaRobots robot, Tools tool)
+     {
+      auto moduleName = ModuleNameFromParams(robot, tool); 
+      auto rm = mc_rbdyn::RobotLoader::get_robot_module(moduleName);
+      if(rm->mb.nrDof() == 0)
+      {
+        exit(1);
+      }
+
+      if(tool == Tools::Pump)
+      {
+        if(rm->devices().size() != 2)
+        {
+          mc_rtc::log::error_and_throw("Panda with pump tool should have 2 devices, got {}", rm->devices().size());
+          if(std::find_if(rm->devices().begin(), rm->devices().end(), [](const auto & d) { return d->name() == "panda_pump"; }) == rm->devices().end())
+          {
+              mc_rtc::log::error_and_throw("Panda with pump tool should have a device named panda_pump");
+          }
+        }
+      }
+     });
   return 0;
 }
